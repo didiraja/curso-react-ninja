4bfe1b1bc1208c35122ba7d5fdeba129359596fe
didiraja <didiraja@hotmail.com>
2018-09-12 18:21:23 -0300

    iniciado curso

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class App extends Component {
+  render() {
+    return (
+      <div data-id='1'>
+        <Title name='Dico' />
+      </div>
+    );
+  }
+}
+
+export default App;
