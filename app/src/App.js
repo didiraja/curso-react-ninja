7957bdf75a0e02dae153939bd6dd1567d4bb57cd
didiraja <didiraja@hotmail.com>
2018-09-13 11:10:42 -0300

    feito até M1#A14

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import Square from './Square';
 
 class App extends Component {
   render() {
     return (
       <div data-id='1'>
-        <Title name='Dico' />
+        {['blue', 'red', 'green'].map(
+          (cor, key) => ( <Square key={key} color={cor} /> )
+        )}
       </div>
     );
   }
--- app/src/Title.js
+++ app/src/Title.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class Title extends Component {
-  render() {
-    return (
-      <h1>Olá {this.props.name}!</h1>
-    );
-  }
+
+    render() {
+        return (
+            <h1>{`Olá ${this.props.name} ${this.props.lastname}!`}</h1>
+        );
+    }
 }
+
+Title.defaultProps = {
+    name: "Desconhecido",
+    lastname: "Sem Sobrenome"
+};
 
 export default Title;
