cec61693b04db6f0b374e69e5c1e2a4c62d4347e
didiraja <didiraja@hotmail.com>
2018-09-25 18:21:05 -0300

    concluido explicacao inicial; começando github app

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import Button from './Button';
-import Square from './Square';
-import Timer from './Timer';
+// import Button from './Button';
+// import Square from './Square';
+// import Timer from './Timer';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      checked: false,
+      showContent: false
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Button>Clique em mim</Button>
-      </div>
+      <React.Fragment>
+        <label>
+          <input
+          type="checkbox"
+          checked={this.state.checked}
+          onChange={() => {
+            this.setState({
+              checked: !this.state.checked
+            }, () => {
+            this.setState({
+              showContent: this.state.checked
+            })
+            })
+          }} />
+          Mostrar Conteúdo
+        </label>
+
+        {
+          this.state.showContent &&
+          <div>Olha eu aqui!</div>
+        }
+      </React.Fragment>
     );
   }
 }
